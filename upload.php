bbca09802edd9739470f3e27a700d7d09757faf1
Juan Pablo Solana Ortiz <juanpablosolana@gmail.com>
2017-08-28 15:06:32 -0500

    Update upload.php

--- upload.php
+++ upload.php
@@ -5,20 +5,16 @@ $RE_emisor_n='<.*?Emisor.*?Nombre="(.*?)"';
 $RE_emisor='<.*?Emisor.*?"(.*?)"';
 $RE_fecha='.*?((?:2|1)\d{3}(?:-|\/)(?:(?:0[1-9])|(?:1[0-2]))(?:-|\/)(?:(?:0[1-9])|(?:[1-2][0-9])|(?:3[0-1]))(?:T|\s)(?:(?:[0-1][0-9])|(?:2[0-3])):(?:[0-5][0-9]):(?:[0-5][0-9]))';
 $RE_concepto='<.*?Concepto.*?descripcion="(.*?)".*?>';
-// subiendo el xml al server
+// subiendo el xml al serve
 $target_path = "files/";
-$target_path = $target_path . basename( $_FILES['uploadedfile']['name']); if(move_uploaded_file($_FILES['uploadedfile']['tmp_name'], $target_path)) { echo "El archivo ". basename( $_FILES['uploadedfile']['name']). " ha sido subido";
-} else{
-echo "Ha ocurrido un error, trate de nuevo!";
-}
-// cargando  xml
+$target_path = $target_path . basename( $_FILES['uploadedfile']['name']); if(move_uploaded_file($_FILES['uploadedfile']['tmp_name'], $target_path)) { echo "El archivo ". basename( $_FILES['uploadedfile']['name']). " ha sido subido con exito";
 $xmlCont=file_get_contents($target_path);
-//Extraer fecha
+//Extraer fecha del xml
 preg_match_all("/".$RE_fecha."/is",$xmlCont, $matches);
 $fechaxmlunix=strtotime($matches[1][0]);
 $fechaxmlorig=$matches[1][0];
 unset($matches);
-//Extraer datos del receptor
+//Extraer rfc del receptor
 preg_match_all('/'.$RE_receptor.'/is',$xmlCont, $matches);
 $rfcxmlre=$matches[1][0]; // RFC del receptor
 unset($matches);
@@ -38,10 +34,16 @@ $desxml=implode(", ",$matches[1]); // Descripciones de los conceptos separadas p
 unset($matches);
 //imprimiendo resultados
 echo "<br />";
+echo "<br />";																															
 echo "Nombre del receptor: ".$nombrexmlre."<br />";
 echo "RFC: ".$rfcxmlre."<br />";
+echo "<br />";																															
 echo "Nombre del emisor: ".$nombrexmlem."<br />";
 echo "RFC emisor es: ".$rfcxmlem."<br />";
+echo "<br />";
 echo "Conceptos: ".$desxml."<br />";
-echo "Fecha es: ".$fechaxmlorig."<br />";
+echo "Fecha de la operación: ".$fechaxmlorig."<br />";
+}else{
+echo "Ha ocurrido un error, trate de nuevo!";
+}
 ?>
